5c9134960415eaae56d613fe77ef456e78a32383
anishkarki23 <anishkarki23@gmail.com>
2022-08-12 14:44:35 +0200

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Python'},
-    {'id':2, 'name':'Java'},
-    {'id':3, 'name':'C++'},
-]
+#rooms = [
+#   {'id':1, 'name':'Python'},
+#    {'id':2, 'name':'Java'},
+#    {'id':3, 'name':'C++'},
+#]
 
 
 def home(request):
+    
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context )
 
 def room(request, pk):
-    #pk parameter will pass the id value in the room 
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room':room}
 
+    #pk parameter will pass the id value in the room 
+    #room = None
+    #for i in rooms:
+    #    if i['id'] == int(pk):
+    #        room = i
+
+
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
     return render(request, 'base/room.html', context )
